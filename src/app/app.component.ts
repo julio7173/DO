95109a73ec1e939a96328e0ddb5063b264efe870
julio7173 <juliocesarsevericheorellana@gmail.com>
2024-07-03 13:49:57 -0400

    Test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent, ListComponent],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Teaching';
-
-  loggedIn = true;
-
-  toggleLog(){
-    this.loggedIn = !this.loggedIn;
-  }
 }
